44a50c435f9b36a172ac482feec7f36ced52f5d1
Ultimarr <hudayullah12@gmail.com>
2025-06-05 12:55:05 +0700

    All of it

---
+++ paketGraph.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+Dibuat oleh:
+1. Hudayullah Ayasya Khoirizal
+2. Muhammad Hanif Awliya
+3. Aisya Citra Hapsari
+
+main                 : Hudayullah
+addressChecker       : Hudayullah
+initGraph            : Hudayullah
+Breadth First Search :
+kalo ada function lain jangan lupa di list disini
+*/
+
+int **matrix;
+bool **isSetMatrix;
+int numOfNodes;
+bool directed;
+bool weighted;
+
+void initGraph(int nodes, bool isDirected, bool isWeighted)
+{
+    numOfNodes = nodes;
+    directed = isDirected;
+    weighted = isWeighted;
+
+    matrix = new int *[numOfNodes];
+    isSetMatrix = new bool *[numOfNodes];
+
+    for (int i = 0; i < numOfNodes; i++)
+    {
+        matrix[i] = new int[numOfNodes];
+        isSetMatrix[i] = new bool[numOfNodes];
+        for (int j = 0; j < numOfNodes; j++)
+        {
+            matrix[i][j] = 0;
+            isSetMatrix[i][j] = false;
+        }
+    }
+}
+
+bool addressChecker(char Address)
+{
+    if (Address < 'a' || Address > 'o')
+    {
+        cout << "Alamat tidak valid" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    initGraph(15, false, true);
+
+    string buyerName;
+    string sellerName;
+    string itemBought;
+    char buyerAddress;
+    char sellerAddress;
+
+    cout << R"(
+              7            3          1
+        a ------------ f ----- g ---------- m
+        |\             |      / \           |
+        |  \  9        |     /   \3         |5
+      3 |    ----      |    /2    \     8   |
+        |       |  2   |   /       j ------ k
+        b ----- e ---- h----------/         |
+        |       |      |      5             |
+      4 |       |2     |7                   |9
+        |  8    |   5  |   7      9      7  |
+        c ----- d ---- i ---- l ----- o --- n
+    )";
+
+    cout << "\nInputkan nama pembeli\t: ";
+    getline(cin, buyerName);
+    cout << "Inputkan apa yang dibeli: ";
+    getline(cin, itemBought);
+    cout << "Inputkan alamat pembeli\t: ";
+    cin >> buyerAddress;
+    if (!addressChecker(buyerAddress))
+    {
+        return 1;
+    }
+    cin.ignore();
+    cout << "Inputkan nama penjual\t: ";
+    getline(cin, sellerName);
+    cout << "Inputkan alamat penjual\t: ";
+    cin >> sellerAddress;
+    if (!addressChecker(sellerAddress))
+    {
+        return 1;
+    }
+
+    cout << "\nBerikut adalah pengisian data: \n";
+    cout << "Nama Pembeli\t: " << buyerName << endl;
+    cout << "Membeli\t\t: " << itemBought << endl;
+    cout << "Alamat Pembeli\t: " << buyerAddress << endl;
+    cout << "Nama Penjual\t: " << sellerName << endl;
+    cout << "Alamat Penjual\t: " << sellerAddress << endl;
+
+    return 0;
+}
